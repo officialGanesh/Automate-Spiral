56334aa5d8b88ca055714aa9a5d49f6a03a3e9fd
Ganesh <ganuofficial2795@gmail.com>
2021-05-14 19:17:22 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Import the required modules
+import pyautogui as gui
+
+DISTANCE = 300
+CHANGE = 10
+
+def Spiral_draw(distance,change):
+    """Function to draw spiral"""
+
+    
+
+
+if __name__ == "__main__":
+
+    Spiral_draw(DISTANCE,CHANGE)
+
+    print("Code Completed 🔥")
