6e751c84a2cdea4b8a6cda4a1c2eb4441ea3f249
Ganesh <ganuofficial2795@gmail.com>
2021-05-14 19:58:14 +0530

    Path defined

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from time import sleep
 
 DISTANCE = 300
 CHANGE = 10
+PATH = r"#" # Write your saving directory
 
 def Spiral_draw(distance,change):
     """Function to draw spiral"""
+
 
     print("Starting In 3 2 1 😮")
     sleep(3)
@@ -22,9 +24,21 @@ def Spiral_draw(distance,change):
         distance -= change
         gui.drag(0,-distance,duration=0.25) # Up 
 
+def Define_path(path):
+    """Defining path to save the file"""
 
+    try:    
+
+        gui.hotkey('ctrl','s')
+        gui.write(f"{path}\spiral.jpg")
+        gui.click(1398,793)
+
+    except Exception as e:
+        print('Something Went Wrong :', e)
+    
 
 if __name__ == "__main__":
 
     Spiral_draw(DISTANCE,CHANGE)
+    Define_path(PATH)
     print("Code Completed 🔥")
